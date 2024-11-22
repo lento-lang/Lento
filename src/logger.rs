4105cf52cb81d36e932ba46b8ee9bee07faa59a8
WilliamRagstad <william.ragstad@gmail.com>
2024-11-22 20:17:25 +0100

    Update logger.rs

--- src/logger.rs
+++ src/logger.rs
@@ -3,12 +3,34 @@
 
 use env_logger::Builder;
 use log::LevelFilter;
+use std::io::Write;
 
 use crate::error::print_error;
+
+const BRIGHT_BLACK: &str = "\x1b[90m";
+const RESET: &str = "\x1b[0m";
 
 pub fn init_logger(level: LevelFilter) {
     Builder::new()
         .filter_level(level)
+        .default_format()
+        .format(|buf, record| {
+            write!(buf, "{BRIGHT_BLACK}[{RESET}")?;
+            write!(
+                buf,
+                "{style}{lvl}{style:#}",
+                lvl = record.level(),
+                style = buf.default_level_style(record.level()),
+            )?;
+            write!(
+                buf,
+                " {}:{}",
+                record.target(),
+                record.line().unwrap_or_default()
+            )?;
+            write!(buf, "{BRIGHT_BLACK}]{RESET}")?;
+            writeln!(buf, " {}", record.args())
+        })
         .target(env_logger::Target::Stdout)
         .init()
 }
