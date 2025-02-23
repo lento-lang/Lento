542e32cc5d9df8c5f46090d7053d78f0c1ecb892
WilliamRagstad <william.ragstad@gmail.com>
2025-02-23 17:05:16 +0100

    Cleanup

--- core/src/parser/ast.rs
+++ core/src/parser/ast.rs
@@ -43,75 +43,57 @@ impl PartialEq for TypeAst {
 #[derive(Debug, Clone)]
 pub enum Ast {
     /// A literal is a constant value that is directly represented in the source code.
-    /// 1. Value of the literal
     Literal { value: Value, info: LineInfo },
     /// A tuple is a fixed-size collection of elements of possibly different types.
-    /// 1. List of elements
     Tuple { exprs: Vec<Ast>, info: LineInfo },
     /// A dynamic list of elements.
-    /// 1. List of elements
-    /// 2. Type of every element in the list (all elements must **be a subtype**)
     List { exprs: Vec<Ast>, info: LineInfo },
-    /// A record is a collection of key-value pairs
-    /// 1. List of key-value pairs
+    /// A record is a collection of key-value fields.
     Record {
         fields: Vec<(RecordKey, Ast)>,
         info: LineInfo,
     },
-    /// An identifier is a named reference to a value in the environment
-    /// 1. Name of the identifier
+    /// An identifier is a named reference to a value in the environment.
     Identifier { name: String, info: LineInfo },
-    /// An assignment expression assigns a value to a variable
-    /// 1. Matching pattern (identifier, destructuring of a tuple, record, etc.)
-    /// 2. Value
+    /// An assignment expression assigns a value to a variable via a matching pattern (identifier, destructuring of a tuple, record, etc.).
     Assignment {
         target: Box<Ast>,
         expr: Box<Ast>,
         info: LineInfo,
     },
-    /// A function declaration is a named function with a list of parameters and a body expression
+    /// A function declaration is a named function with a list of parameters and a body expression.
     FunctionDef {
         param: ParamAst,
         body: Box<Ast>,
         return_type: Option<TypeAst>,
         info: LineInfo,
     },
-    /// A function call is an invocation of a function with a list of arguments
-    /// 1. Name of the function
-    /// 2. List of arguments
+    /// A function call is an invocation of a function with a list of arguments.
     FunctionCall {
         expr: Box<Ast>,
         arg: Box<Ast>,
         info: LineInfo,
     },
-    /// An accumulate expression is an operation with multiple operands
-    /// 1. Operator
-    /// 2. List of operands
+    /// An accumulate expression is an operation with multiple operands.
     Accumulate {
         op_info: OperatorInfo,
         exprs: Vec<Ast>,
         info: LineInfo,
     },
-    /// A binary expression is an operation with two operands
-    /// 1. Left operand
-    /// 2. Operator
-    /// 3. Right operand
+    /// A binary expression is an operation with two operands.
     Binary {
         lhs: Box<Ast>,
         op_info: OperatorInfo,
         rhs: Box<Ast>,
         info: LineInfo,
     },
-    /// A unary expression is an operation with one operand
-    /// 1. Operator
-    /// 2. Operand
+    /// A unary expression is an operation with one operand.
     Unary {
         op_info: OperatorInfo,
         expr: Box<Ast>,
         info: LineInfo,
     },
     /// Block expression evaluates all expressions in the block and returns the value of the last expression.
-    /// 1. List of expressions
     Block(Vec<Ast>, LineInfo),
 }
 
