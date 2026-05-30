da681420595dc283f4b64b2a9818e9eb447cb6a3
William <william.ragstad@gmail.com>
2026-05-30 18:57:11 +0200

    Hide placeholder CLI commands

--- lentoc/src/main.rs
+++ lentoc/src/main.rs
@@ -21,18 +21,8 @@ fn main() {
     }
 
     match args.subcommand() {
-        Some((lento_command::BUILD, _)) => println!("The build command is not yet implemented!"),
-        Some((lento_command::COMPILE, _)) => {
-            println!("The compile command is not yet implemented!")
-        }
-        Some((lento_command::DOC, _)) => println!("The doc command is not yet implemented!"),
         Some((lento_command::EVAL, args)) => handle_command_eval(args, &mut arg_parser),
-        Some((lento_command::FMT, _)) => println!("The fmt command is not yet implemented!"),
-        Some((lento_command::LINT, _)) => println!("The lint command is not yet implemented!"),
         Some((lento_command::REPL, args)) => handle_command_repl(args, &mut arg_parser),
-        Some((lento_command::RUN, _)) => println!("The run command is not yet implemented!"),
-        Some((lento_command::NEW, _)) => println!("The new command is not yet implemented!"),
-        Some((lento_command::TEST, _)) => println!("The test command is not yet implemented!"),
         _ => match args.get_one::<String>("file") {
             Some(raw_file) => handle_command_file(raw_file),
             _ => {
